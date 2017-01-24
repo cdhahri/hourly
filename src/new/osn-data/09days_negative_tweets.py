20c183481c516ac6df9ce6567400b0567cfb94fd
cdhahri <chaima.aldhahri@gmail.com>
2017-01-24 14:41:49 +0000

    m

--- src/new/osn-data/09days_negative_tweets.py
+++ src/new/osn-data/09days_negative_tweets.py
@@ -19,7 +19,7 @@ def process(r, target_path, w):
   i = -1
   for key in sorted(tweets.keys()):
     i += 1
-    if targets[i] == '0':# and tweets[key]['source'] in sources:
+    if targets[i] == '-1':# and tweets[key]['source'] in sources:
       # Tue Sep 27 01:58:41 +0000 2016
       current_day = tweets[key]['created_at']
       current_day_object = datetime.strptime(current_day, '%a %b %d %H:%M:%S %z %Y')
